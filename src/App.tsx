fa91b1e3f631ed2213b51d377b537166759bd670
Matheus <matheus@matheusferreira.dev>
2024-08-25 21:16:39 -0300

    feat: added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from './styles/global';
 
 import { defaultTheme } from './styles/themes/default';
 
+import { Router } from './Router';
+
 export function App() {
-  return <ThemeProvider theme={defaultTheme}>
-    <GlobalStyle />
-  </ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
